60eb9675120ad9276b3d3a8fac73cf6c4e8da285
Lokeshwar Deb <lokeshwardebdevelop@gmail.com>
2023-09-15 20:42:58 +0600

    added the phpmailer and uploaded it to github

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'personal_management_website/personal_management_website',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '580ea81508c07c50fb1564cbf726e15d131727cd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'personal_management_website/personal_management_website' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '580ea81508c07c50fb1564cbf726e15d131727cd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.1',
+            'version' => '6.8.1.0',
+            'reference' => 'e88da8d679acc3824ff231fdc553565b802ac016',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
